80a9f31f1645f716c2e63ec9569685049c4ba06e
gitest01 <mahan89@daum.net>
2018-09-15 15:03:14 +0900

    이제 시작

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: "Winstorm"
+  };
+
+  isSpecial = true;
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+  onSave(event: any) {
+    console.log(event);
+    // this.isSpecial = !this.isSpecial; // toggle
+  }
+
+}
